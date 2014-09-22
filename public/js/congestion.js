4f88f65480e4d1dc61abd9a32f8efb0633755763
jcdoig <jcdoig@gmail.com>
2014-09-22 15:51:08 -0700

    Density Comparison

--- public/js/congestion.js
+++ public/js/congestion.js
@@ -676,7 +676,7 @@ window.onload = function() {
           //console.log(d.center_radius, xScale(d.center_radius)); return 10;
           return comCircleScale(d.center_radius)
         }).on("click",function(d){
-          community_plot(d)
+          community_plot.update(d)
         })
 
         return node
@@ -1313,7 +1313,7 @@ window.onload = function() {
       window.community_plot = function() {
         var figure_interfase
 
-        var margin = {top: 20, right: 80, bottom: 30, left: 50},
+        var margin = {top: 30, right: 80, bottom: 20, left: 50},
             width = 960 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
 
@@ -1321,6 +1321,9 @@ window.onload = function() {
             .range([0, width]);
 
         var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var density_y = d3.scale.linear()
             .range([height, 0]);
 
         var color = d3.scale.category10();
@@ -1333,10 +1336,19 @@ window.onload = function() {
             .scale(y)
             .orient("left");
 
+        var density_yAxis = d3.svg.axis()
+            .scale(density_y)
+            .orient("right");
+
         var line = d3.svg.line()
             .interpolate("basis")
             .x(function(d) { return x(d.step); })
             .y(function(d) { return y(d.indexHomoCongestion); });
+
+        var density_line = d3.svg.line()
+            .interpolate("basis")
+            .x(function(d) { return x(d.step); })
+            .y(function(d) { return density_y(calculateDensity(d.avg_speed_avg)); });
 
         var svg = d3.select("body").append("svg")
             .attr("width", width + margin.left + margin.right)
@@ -1344,9 +1356,16 @@ window.onload = function() {
           .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+        svg.append("rect").attr("width",210).attr("height",20).attr("y",-25).style("fill","steelblue")
+          .on("click",load_microsimulation)
+          .style("cursor","pointer")
+
+        svg.append("text").attr("x",10).attr("y",-10).style("fill","white").text("to Load the microsimulation data click here")
+          .style("pointer-events","none")
         // x.domain(d3.extent(communities, function(d) { return +d.key; }));
         x.domain([10,1300]);
         y.domain([0,1]);
+        density_y.domain([0,160]);
 
         svg.append("g")
             .attr("class", "x axis")
@@ -1361,9 +1380,100 @@ window.onload = function() {
             .attr("y", 6)
             .attr("dy", ".71em")
             .style("text-anchor", "end")
-            .text("Index of Homogeneous Congestion");
+            .text("Index of Homogeneous Congestion")
+            .style("fill","steelblue");
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(" + width + ",0)")
+            .call(density_yAxis)
+          .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "-1em")
+            .style("text-anchor", "end")
+            .text("density in veh/mi")
+            .style("fill","red");
+
+        var micro_simulation_data
+        var readDensity
+        var selected_community
+        var read_density = function(time, links) {
+          return readDensity(time, links)
+        }
+
+        function load_microsimulation(){
+          if (micro_simulation_data == undefined) {
+            var micro_q = queue(1)
+
+            var files = d3.range(1,90)
+              .map(function(d){ return "Manhattan/Microsimulation/rawData_v5."+d+".json"})
+            
+            files.forEach(function(t) { micro_q.defer(loadJSON, rootUrl + t); });
+
+            micro_q.awaitAll(function(error, results) { 
+              console.log("all files downloaded!, now pre-processing");
+              // console.log(results); 
+              micro_simulation_data = []
+              results.forEach(function(d){
+                micro_simulation_data = micro_simulation_data.filter(function(entry){return entry.type == "sec"}).concat(d)
+              })
+              console.log(micro_simulation_data)
+              var micro_nest = d3.nest()
+                .key(function(d){ return d.time})
+                .entries(micro_simulation_data)
+
+              console.log(micro_nest)
+
+              readDensity = function(time, links){
+                  console.log((Math.floor((+time + 1800)/120)*120))
+                  var timeData = micro_nest.filter(function(d){return d.key == (Math.floor((+time + 1800)/120)*120).toString()})[0].values
+                  var inLinks = links
+                  var linkData = timeData.filter(function(d){return inLinks.indexOf(d.id.toString()) >= 0})
+                  return d3.mean(linkData.map(function(d){return d.density}))
+              }
+
+              if (selected_community != undefined){
+
+                figure_interfase(selected_community)
+              }
+            });  
+          } else {
+            console.log("data already loaded")
+          }
+          
+        }
+
+        function calculateDensity(speed){
+          //uses the MFD to estimate the density of the network
+          var pwlinear = []
+          if (26.8 <= speed) {
+            pwlinear = [0,0,15,250]
+          }
+          if (21.11 <= speed && speed < 26.8) {
+            pwlinear = [15,250,40,525]
+          }
+          if (14.08 <= speed && speed < 21.11) {
+            pwlinear = [40, 525,60,525]
+          }
+          if (10.56 <= speed && speed < 14.08) {
+            pwlinear = [60,525,80,525]
+          }
+          if (speed < 10.56) {
+            pwlinear = [80,525,220,125]
+          }
+          if (pwlinear.length > 0){ 
+            return (pwlinear[1]-(pwlinear[3]-pwlinear[1])/(pwlinear[2]-pwlinear[0])*pwlinear[0])/(speed/1.609 - ((pwlinear[3]-pwlinear[1])/(pwlinear[2]-pwlinear[0])))
+          } else {
+            console.log(speed)
+            return -1
+          }
+        }
 
         figure_interfase = function(community){
+          console.log(community)
+
+          selected_community = community
           var com_id = community.com_id;
 
           var data = communities.map(function(d){
@@ -1378,28 +1488,8 @@ window.onload = function() {
             return 
           }).filter(function(d){return d != undefined})
 
-          
-          
-          // x.domain(d3.extent(communities, function(d) { return +d.key; }));
-          // y.domain([0,1]);
-
-
-          // svg.append("g")
-          //     .attr("class", "x axis")
-          //     .attr("transform", "translate(0," + height + ")")
-          //     .call(xAxis);
-
-          // svg.append("g")
-          //     .attr("class", "y axis")
-          //     .call(yAxis)
-          //   .append("text")
-          //     .attr("transform", "rotate(-90)")
-          //     .attr("y", 6)
-          //     .attr("dy", ".71em")
-          //     .style("text-anchor", "end")
-          //     .text("index");
-
           svg.selectAll(".data-path").remove()
+          svg.selectAll(".data-points").remove()
 
           svg.append("g").attr("class","data-path")
             .append("path")
@@ -1408,62 +1498,31 @@ window.onload = function() {
               .attr("d", function(d) { return line(d); })
               .style("stroke", "steelblue")
               .style("fill","none");
+
+          svg.append("g").attr("class","data-path")
+            .append("path")
+              .datum(data)
+              .attr("class", "line")
+              .attr("d", function(d) { return density_line(d); })
+              .style("stroke", "red")
+              .style("fill","none");
+
+          if (micro_simulation_data != undefined) {
+            
+
+            svg.append("g").attr("class","data-points").selectAll("circle").data(data.map(function(d){
+              var micro_density = readDensity(d.step, d.links)
+              return {step: d.step, density:micro_density}
+            }))
+            .enter().append("circle")
+              .attr("r",10)
+              .attr("cx",function(d){return x(d.step)})
+              .attr("cy",function(d){return density_y(d.density)}) 
+              .style("fill","red") 
+          }
+          
         }
-        // d3.tsv("data.tsv", function(error, data) {
-        //   color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
 
-        //   data.forEach(function(d) {
-        //     d.date = parseDate(d.date);
-        //   });
-
-        //   var cities = color.domain().map(function(name) {
-        //     return {
-        //       name: name,
-        //       values: data.map(function(d) {
-        //         return {date: d.date, temperature: +d[name]};
-        //       })
-        //     };
-        //   });
-
-        //   x.domain(d3.extent(data, function(d) { return d.date; }));
-
-        //   y.domain([
-        //     d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
-        //     d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
-        //   ]);
-
-        //   svg.append("g")
-        //       .attr("class", "x axis")
-        //       .attr("transform", "translate(0," + height + ")")
-        //       .call(xAxis);
-
-        //   svg.append("g")
-        //       .attr("class", "y axis")
-        //       .call(yAxis)
-        //     .append("text")
-        //       .attr("transform", "rotate(-90)")
-        //       .attr("y", 6)
-        //       .attr("dy", ".71em")
-        //       .style("text-anchor", "end")
-        //       .text("Temperature (ºF)");
-
-        //   var city = svg.selectAll(".city")
-        //       .data(cities)
-        //     .enter().append("g")
-        //       .attr("class", "city");
-
-        //   city.append("path")
-        //       .attr("class", "line")
-        //       .attr("d", function(d) { return line(d.values); })
-        //       .style("stroke", function(d) { return color(d.name); });
-
-        //   city.append("text")
-        //       .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-        //       .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-        //       .attr("x", 3)
-        //       .attr("dy", ".35em")
-        //       .text(function(d) { return d.name; });
-        // });
-        return figure_interfase
+        return {update:figure_interfase, load:load_microsimulation, read_density: read_density}
       }();
     }
